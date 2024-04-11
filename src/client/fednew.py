f42f5c8029b90133d20ab2da613b851592b7eeef
Miracle <872137422@qq.com>
2024-04-11 17:02:28 +0800

    add fednew

--- src/client/fednew.py
+++ src/client/fednew.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+from typing import Union, Tuple, List, Dict
+
 import torch
 
 from fedavg import FedAvgClient
@@ -26,3 +29,53 @@ class FedNewClient(FedAvgClient):
                     w.grad.data += self.args.mu * (w.data - w_t.data)
                     # w.grad.data += self.args.mu * torch.sin(w.data - w_t.data)
                 self.optimizer.step()
+
+    def train(
+        self,
+        client_id: int,
+        local_epoch: int,
+        new_parameters: OrderedDict[str, torch.Tensor],
+        return_diff=True,
+        verbose=False,
+    ) -> Tuple[Union[OrderedDict[str, torch.Tensor], List[torch.Tensor]], int, Dict]:
+        """
+        The funtion for including all operations in client local training phase.
+        If you wanna implement your method, consider to override this funciton.
+
+        Args:
+            client_id (int): The ID of client.
+
+            local_epoch (int): The number of epochs for performing local training.
+
+            new_parameters (OrderedDict[str, torch.Tensor]): Parameters of FL model.
+
+            return_diff (bool, optional):
+            `True`: to send the differences between FL model parameters that before and after training;
+            `False`: to send FL model parameters without any change.  Defaults to True.
+
+            verbose (bool, optional): Set to `True` for print logging info onto the stdout (Controled by the server by default). Defaults to False.
+
+        Returns:
+            Tuple[Union[OrderedDict[str, torch.Tensor], List[torch.Tensor]], int, Dict]:
+            [The difference / all trainable parameters, the weight of this client, the evaluation metric stats].
+        """
+        self.client_id = client_id
+        self.local_epoch = local_epoch
+        self.load_dataset()
+        self.set_parameters(new_parameters)
+        eval_results = self.train_and_log(verbose=verbose)
+
+        if return_diff:
+            delta = OrderedDict()
+            for (name, p0), p1 in zip(
+                new_parameters.items(), trainable_params(self.model)
+            ):
+                delta[name] = p0 - p1
+
+            return delta, len(self.trainset), eval_results
+        else:
+            return (
+                trainable_params(self.model, detach=True),
+                len(self.trainset),
+                eval_results,
+            )
--- src/server/fednew.py
+++ src/server/fednew.py
@@ -1,5 +1,7 @@
 from argparse import Namespace, ArgumentParser
+from collections import OrderedDict
 from copy import deepcopy
+from typing import Type, Any
 
 from src.client.fednew import FedNewClient
 from src.server.fedavg import FedAvgServer, get_fedavg_argparser
@@ -23,6 +25,44 @@ class FedNewServer(FedAvgServer):
         if args is None:
             args = get_fednew_argparser().parse_args()
         super().__init__(algo, args, unique_model, default_trainer)
+        self.cluster_params_dict = OrderedDict[
+            # TOD
+        ]
         self.trainer = FedNewClient(
             deepcopy(self.model), self.args, self.logger, self.device
         )
+
+    def train_one_round(self):
+        """The function of indicating specific things FL method need to do (at server side) in each communication round."""
+        delta_cache = []
+        weight_cache = []
+        for client_id in self.selected_clients:
+            client_local_params = self.generate_client_params(client_id)
+            (
+                delta,
+                weight,
+                self.client_metrics[client_id][self.current_epoch],
+            ) = self.trainer.train(
+                client_id=client_id,
+                local_epoch=self.clients_local_epoch[client_id],
+                new_parameters=client_local_params,
+                verbose=((self.current_epoch + 1) % self.args.verbose_gap) == 0,
+            )
+
+            delta_cache.append(delta)
+            weight_cache.append(weight)
+
+        self.aggregate(delta_cache, weight_cache)
+
+
+    def generate_cluster_params(self) -> Type[OrderedDict[Any]]:
+        """
+        This function is for outputting model parameters that asked by `client_id`.
+
+        Args:
+            client_id (int): The ID of query client.
+
+        Returns:
+            OrderedDict[str, torch.Tensor]: The trainable model parameters.
+        """
+        return self.cluster_params_dict
